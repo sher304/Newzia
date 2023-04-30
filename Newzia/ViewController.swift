e9a05db5e73e9b9fa58bd525cb857c9ae3fb7c95
sher304 <esherovshermat@gmail.com>
2023-04-30 17:40:39 +0600

    🎉 Start new projecty

---
+++ Newzia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newzia
+//
+//  Created by Шермат Эшеров on 30/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
